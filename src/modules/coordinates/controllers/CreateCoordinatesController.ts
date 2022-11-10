fa46af790b7141c9dc0d15ce41be5892e38ac52f
DanLucena <danlucas1711@gmail.com>
2022-11-10 00:33:27 -0300

    [+] Adding coordinates

--- src/modules/coordinates/controllers/CreateCoordinatesController.ts
+++ src/modules/coordinates/controllers/CreateCoordinatesController.ts
@@ -1,21 +1,24 @@
 import { Request, Response } from "express";
+import { CollaboratorRepository } from "../../collaborator/repositories/CollaboratorRepository";
 import { CoordinatesRepository } from "../repositories/CoordinatesRepository";
 import { CreateCoordinateService } from "../services/CreateCoordinateService";
 
 class CreateCoordinatesController {
   async handle(request: Request, response: Response): Promise<Response> {
     const coordinatesRepository = new CoordinatesRepository();
+    const collaboratorRepository = new CollaboratorRepository();
     const createCoordinatesService = new CreateCoordinateService(
       coordinatesRepository
     );
 
-    const { x, y, collaboratorId, adjacencyList } = request.body;
+    const { x, y, email } = request.body;
+
+    const collaborator = await collaboratorRepository.findByEmail(email);
 
     const coordinate = await createCoordinatesService.perform({
       x,
       y,
-      collaboratorId,
-      adjacencyList,
+      collaboratorId: collaborator.id,
     });
 
     return response.json(coordinate);
